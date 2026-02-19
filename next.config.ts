0cef01a322d840033ca9d63cefcfbd4ec75e6831
mohammed iqbal <moham.iqbal99@gmail.com>
2026-02-19 11:47:46 +0000

    make the limit 50mb

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   serverActions: {
-    bodySizeLimit: '5mb',
+    bodySizeLimit: '50mb',
   },
   typescript: {
     ignoreBuildErrors: true,
